c786c24f815c321a087d0f704d7ab90465912006
federico figueroa <federicofiguero011@gmail.com>
2022-05-29 19:26:20 -0300

    update

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ app.set("view engine", "ejs")
 // Mongo Atlas
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 }, () => {
     console.log('MongoDB connected...')
 })
